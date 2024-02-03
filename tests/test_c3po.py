b543665c52be949ad917633def734f8242dbac98
Anthony Bolton <anthony.bolton21@ic.ac.uk>
2024-02-03 02:38:29 +0000

    comment edit

--- tests/test_c3po.py
+++ tests/test_c3po.py
@@ -5,7 +5,6 @@ from c3po.c3po import C3PO
 class TestC3PO(unittest.TestCase):
     
     def setUp(self):
-        # Common setup for the millennium falcon data
         self.millennium_falcon_data = {
             "autonomy": 6, 
             "routes": [
@@ -18,7 +17,7 @@ class TestC3PO(unittest.TestCase):
         }
 
     def test_example_1(self):
-        # Setup for Example 1
+        
         empire_data = {
             "countdown": 7,
             "bounty_hunters": [
@@ -32,19 +31,11 @@ class TestC3PO(unittest.TestCase):
         # Create an instance of C3PO and calculate the odds
         c3po = C3PO(self.millennium_falcon_data)
         
-        # Start timing
-        start_time = time.time()
-        actual_odds = c3po.give_me_the_odds(empire_data)
-        # End timing
-        end_time = time.time()
-
-        # Print the runtime
-        print(f"Runtime of test_example_1: {end_time - start_time} seconds")
         # Assert that the actual odds match the expected odds
         self.assertEqual(actual_odds, expected_odds)
 
     def test_example_2(self):
-        # Setup for Example 2
+        
         empire_data = {
             "countdown": 8,
             "bounty_hunters": [
@@ -53,12 +44,11 @@ class TestC3PO(unittest.TestCase):
                 {"planet": "Hoth", "day": 8}
             ]
         }
-        # Expected result for Example 2
+        
         expected_odds = 0.81
-        # Test code similar to test_example_1
 
     def test_example_3(self):
-        # Setup for Example 3
+        
         empire_data = {
             "countdown": 9,
             "bounty_hunters": [
@@ -67,12 +57,11 @@ class TestC3PO(unittest.TestCase):
                 {"planet": "Hoth", "day": 8}
             ]
         }
-        # Expected result for Example 3
+        
         expected_odds = 0.9
-        # Test code similar to test_example_1
 
     def test_example_4(self):
-        # Setup for Example 4
+        
         empire_data = {
             "countdown": 10,
             "bounty_hunters": [
@@ -81,10 +70,8 @@ class TestC3PO(unittest.TestCase):
                 {"planet": "Hoth", "day": 8}
             ]
         }
-        # Expected result for Example 4
+        
         expected_odds = 1
-        # Test code similar to test_example_1
 
-# This allows the tests to be run from the command line
 if __name__ == '__main__':
     unittest.main()
